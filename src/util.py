b7533f35974983e5a575f585054abb25341a6ece
AdamOlsson <Olsson.95_@hotmail.com>
2020-02-26 11:27:54 +0100

    added optimized version of generate perspectives

--- src/util.py
+++ src/util.py
@@ -101,3 +101,49 @@ def shift_state(row, col, previous_state, state, grid_shift):
         return previous_perspective, perspective
 
 
+
+def generatePerspectiveOptimized(grid_shift, toric_size, state):
+    """ Generates the perspectives for a given syndrom.
+
+    Params
+    ======
+    grid_shift: (int)
+    toric_size: (int)
+    state:      (np.ndarray)
+
+    Return
+    ======
+    (np.ndarray) All the perspectives
+    """
+    def mod(index, shift):
+        index = (index + shift) % toric_size 
+        return index
+    perspectives = []
+    vertex_matrix = state[0,:,:]
+    plaquette_matrix = state[1,:,:]
+    # qubit matrix 0
+    vme0 = np.where(   vertex_matrix == 1, 1, 0)
+    pme0 = np.where(plaquette_matrix == 1, 1, 0)
+    vme1 = np.where(np.roll(  vertex_matrix, -1, axis=0), 1, 0)
+    pme1 = np.where(np.roll(plaquette_matrix, 1, axis=0), 1, 0)
+    err  = np.logical_or.reduce(np.array([vme0, vme1, pme0, pme1]))
+    args = np.argwhere(err == 1)
+    for (i,j) in args:
+        new_state = np.roll(state, grid_shift-i, axis=1)
+        new_state = np.roll(new_state, grid_shift-j, axis=2)
+        perspectives.append(Perspective(new_state, (0,i,j)))
+
+    # qubit matrix 1
+    vme0 = np.where(   vertex_matrix == 1, 1, 0)
+    pme0 = np.where(plaquette_matrix == 1, 1, 0)
+    vme1 = np.where(np.roll(  vertex_matrix, -1, axis=1), 1, 0)
+    pme1 = np.where(np.roll(plaquette_matrix, 1, axis=1), 1, 0)
+    err  = np.logical_or.reduce(np.array([vme0, vme1, pme0, pme1]))
+    args = np.argwhere(err == 1)
+    for (i,j) in args:
+        new_state = np.roll(state, grid_shift-i, axis=1)
+        new_state = np.roll(new_state, grid_shift-j, axis=2)
+        new_state = rotate_state(new_state) # rotate perspective clock wise
+        perspectives.append(Perspective(new_state, (1,i,j)))
+
+    return perspectives
