eda28bdcd677d93b947710d50f05a1fec328c63b
Lindeby <gabriel.lindeby@gmail.com>
2020-03-23 16:16:04 +0100

    tensorboard nvidia-smi setupfile

--- src/IO_mp.py
+++ src/IO_mp.py
@@ -1,6 +1,7 @@
 from src.ReplayMemory import PrioritizedReplayMemory
 import numpy as np
 import time
+import nvgpu
 from datetime import datetime
 
 could_import_tb=True
@@ -11,7 +12,7 @@ except:
     print("Could not import tensorboard. No logging will occur.")
 
 
-def io(memory_args):
+def io(memory_args, actor_args, learner_args):
     
     memory_capacity             = memory_args["capacity"]
     memory_alpha                = memory_args["alpha"]
@@ -35,10 +36,74 @@ def io(memory_args):
         samples_learner = np.zeros(int(tb_priority_sample_max/tb_priority_sample_interval_size))
 
         tb = SummaryWriter(tb_write_dir)
+        tb_nvidia_log_freq = 10 #seconds
 
     replay_memory = PrioritizedReplayMemory(memory_capacity, memory_alpha)
 
-    log_count_actor   = 0
+    if should_log:
+        
+        tb_setup_string = ("env_size: {}  \n"
+                           "learning_rate: {}  \n"
+                           "learner_update_policy: {}  \n"
+                           "learner_optimizer: {}  \n"
+                           "learner_device: {}  \n"
+                           "learner_job_max_time: {}  \n"
+                           "learner_save_date: {}  \n"
+                           "learner_eval_no_episodes: {}  \n"
+                           "learner_eval_freq: {}  \n"
+                           "actor_max_actions_per_episode: {}  \n"
+                           "actor_size_local_memory_buffer: {}  \n"
+                           "actor_no_envs: {}  \n"
+                           "no_cuda_actors: {}  \n"
+                           "no_cpu_actors: {}  \n"
+                           "env_p_error_interval_start: {}  \n"   
+                           "env_p_error_interval_final: {}  \n"   
+                           "env_p_error_interval_increase: {}  \n"
+                           "env_p_error_strategy: {}  \n"
+                           "replay_memory_size: {}  \n"                            
+                           "replay_memory_alpha: {}  \n"                 
+                           "replay_memory_beta: {}  \n"                  
+                           "replay_memory_size_before_sampeling: {}  \n" 
+                           "replay_memory_batch_in_queue_limit: {}  \n"  
+                           "log_priority_dist: {}  \n"                   
+                           "log_write_frequency: {}  \n"                 
+                           "log_priority_sample_max: {}  \n"             
+                           "log_priority_sample_interval_size: {}  \n"
+                           "batch_size: {}  \n"
+                           "discount_factor: {}  \n").format(learner_args["env_config"]["size"],
+                                                             learner_args["learning_rate"],
+                                                             learner_args["policy_update"],
+                                                             learner_args["optimizer"],
+                                                             learner_args["device"],
+                                                             learner_args["job_max_time"],
+                                                             learner_args["save_date"],
+                                                             learner_args["learner_eval_no_episodes"],
+                                                             learner_args["learner_eval_freq"],
+                                                             actor_args["max_actions_per_episode"],
+                                                             actor_args["size_local_memory_buffer"],
+                                                             actor_args["no_envs"],
+                                                             actor_args["no_cuda_actors"],
+                                                             actor_args["no_cpu_actors"],
+                                                             actor_args["env_p_error_start"],
+                                                             actor_args["env_p_error_final"],
+                                                             actor_args["env_p_error_delta"],
+                                                             actor_args["env_p_error_strategy"],
+                                                             memory_args["capacity"],
+                                                             memory_args["alpha"],
+                                                             memory_args["beta"],
+                                                             memory_args["replay_size_before_sampling"],
+                                                             memory_args["batch_in_queue_limit"],
+                                                             memory_args["log_priority_dist"],
+                                                             memory_args["log_write_frequency",
+                                                             memory_args["log_priority_sample_max"],
+                                                             memory_args["log_priority_sample_interval_size"],
+                                                             memory_args["batch_size"],
+                                                             actor_args["discount_factor"])
+
+        print(tb_setup_string)
+
+
+    log_count_actor   = 0     
     log_count_learner = 0
     count_gen_trans   = 0
     count_cons_trans  = 0
@@ -46,6 +111,7 @@ def io(memory_args):
     count_total_cons_trans = 0
     start_learning = False
     total_amout_transitions = 0
+    nvidia_log_time = time.time()
     stop_watch = time.time()
     while(True):
 
@@ -78,6 +144,18 @@ def io(memory_args):
                 count_gen_trans  = 0
                 count_cons_trans = 0
                 samples_actor    = np.zeros(int(tb_priority_sample_max/tb_priority_sample_interval_size))
+
+            if should_log and nvidia_log_time + tb_nvidia_log_freq > time.time():
+                nvidia_log_time = time.time() 
+                gpu_info = nvgpu.gpu_info()
+                
+                for i in gpu_info:
+            
+                    gpu = '{} {}'.format(i['type'], i['index'])
+                    mem_total = i['mem_total']
+                    mem_used = i['mem_used'] 
+                    tb.add_scalars(gpu, {'mem_total':mem_total,
+                                           'mem_used':mem_used})
 
 
             
