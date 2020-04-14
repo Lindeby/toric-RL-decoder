7ed1cf62d3c59b1c47915b42e90dd834cd7f8290
AdamOlsson <Olsson.95_@hotmail.com>
2020-04-14 14:44:52 +0200

    various chanhes

--- tests/plotSyndroms.py
+++ tests/plotSyndroms.py
@@ -1,12 +1,40 @@
+import sys
+sys.path.append('/home/adam/Documents/school/thesis/toric-RL-decoder')
+
 import gym, gym_ToricCode
+import numpy as np
+from src.util_actor import generatePerspective
 
 if __name__ == "__main__":
+
+    s = [[[0,0,0,0,0]
+        , [0,0,0,0,0]
+        , [0,0,3,0,0]
+        , [0,0,3,0,0]
+        , [0,0,0,0,0]],
+
+        [ [0,0,0,0,0]
+        , [3,3,0,0,0]
+        , [0,0,0,0,0]
+        , [0,0,0,0,0]
+        , [0,0,0,0,0]]]
+
+    start_error = np.array(s) 
+
     env_config = {  
-        "size": 5,
+        "size": start_error.shape[-1],
         "min_qubit_errors": 0,
         "p_error": 0.1
     }
 
     env = gym.make('toric-code-v0', config=env_config)
-    state = env.reset()
-    env.plotToricCode(state, "plotting")
+
+    env.reset()
+
+
+    state = env.createSyndromOpt(start_error)
+    env.qubit_matrix = start_error
+    env.state = state
+
+    env.plotToricCode(state, 'perspective_'+'2')
+
