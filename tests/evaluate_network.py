276c9b18d1fa5b403b4647fc7cd2b8dab8c6fbe2
AdamOlsson <Olsson.95_@hotmail.com>
2020-04-16 07:59:13 +0200

    pushing CORRECT file for eval of network

--- tests/evaluate_network.py
+++ tests/evaluate_network.py
@@ -3,14 +3,18 @@ import sys
 
 from src.evaluation import evaluate
 from src.nn.torch.NN import NN_11
+from src.nn.torch.ResNet import ResNet18
 import torch
 
 from torch.utils.tensorboard import SummaryWriter
 import numpy as np
 
 if __name__ == "__main__":
+
+    device = 'cuda'
+    no_episodes = 20000 
     
-    env_config = {  "size": 7,
+    env_config = {  "size": 9,
                 "min_qubit_errors": 0,
                 "p_error": 0.1
                 }
@@ -18,22 +22,23 @@ if __name__ == "__main__":
     model_config = {"system_size": env_config["size"],
                     "number_of_actions": env_config["size"]
                     }
-    model = NN_11(model_config["system_size"], 3, 'cpu')
-    model.load_state_dict(torch.load("network/mp/Size_7_NN_11_random_18_Mar_2020_18_17_52.pt", map_location='cpu'))
+    #model = NN_11(model_config["system_size"], 3, device)
+    model = ResNet18()
+    model.load_state_dict(torch.load("runs/14_Apr_2020_19_29_11/Size_9_ResNet_14_Apr_2020_19_29_11.pt", map_location=device))
+    model.to(device)
     model.eval()
 
-    no_episodes = 10000 
     p_error = np.linspace(0.06, 0.2, 8, endpoint=True)
     success_rate, ground_state_rate, average_number_of_steps_list, mean_q_list, failed_syndroms = evaluate( model,
                                                                                                             'toric-code-v0',
                                                                                                             env_config,
                                                                                                             int(env_config["size"]/2),
-                                                                                                            'cpu',
+                                                                                                            device,
                                                                                                             p_error,
                                                                                                             num_of_episodes=no_episodes,
                                                                                                             epsilon=0.0,
                                                                                                             num_of_steps=75,
-                                                                                                            plot_one_episode=True, 
+                                                                                                            plot_one_episode=False, 
                                                                                                             minimum_nbr_of_qubit_errors=0)
 
     tb = SummaryWriter(log_dir='runs/test_size_{}_steps_{}'.format(env_config["size"], no_episodes))
@@ -41,6 +46,8 @@ if __name__ == "__main__":
     for i, p in enumerate(p_error):
         tb.add_scalar("Performance/Ground State Rate", ground_state_rate[i], p*100)
         tb.add_scalar("Performance/Success Rate", success_rate[i], p*100)
+        tb.add_scalar("Performance/Mean Q", mean_q_list[i], p*100)
+        tb.add_scalar("Performance/Avg No Steps", average_number_of_steps_list[i], p*100)
 
 
     tb.close()
